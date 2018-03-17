73a89b0fd215c7dd2622f1c8dcab3d09ae8b2393
Eddie <edison.li777@gmail.com>
2018-03-17 02:18:19 -0500

    To improve the performance, cache rendered bezier path to UIImage

--- Spirograph/Canvas.swift
+++ Spirograph/Canvas.swift
@@ -10,28 +10,91 @@ import Foundation
 import UIKit
 
 
-class Canvas:UIView{
+class Canvas: UIView {
 
-    var pointArray = [CGPoint]()
-
-    func updataPoints(point:CGPoint){
-        
-        pointArray.append(point)
+  var startPoint: CGPoint!
+  var endPoint: CGPoint!
+  var preRenderedImage: UIImage!
+  var bezierPath: UIBezierPath!
+  
+  var pointLimit: Int = 1024
+  var pointCount: Int = 0
+  
+  var lineWidth: CGFloat = 1
+  var lineColor: UIColor = UIColor.black
+  
+  
+  // MARK: - Initialization
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    initBezierPath()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    
+    initBezierPath()
+  }
+  
+  func initBezierPath() {
+    bezierPath = UIBezierPath()
+//    bezierPath.lineCapStyle = CGLineCap.round
+//    bezierPath.lineJoinStyle = CGLineJoin.round
+    bezierPath.lineWidth = lineWidth
+  }
+  
+  
+  func redraw() {
+    bezierPath.move(to: startPoint)
+    bezierPath.addLine(to: endPoint)
+    startPoint = endPoint
+    pointCount += 1
+    
+    if pointCount == pointLimit {
+      pointCount = 0
+      renderToImage()
+      setNeedsDisplay()
+      bezierPath.removeAllPoints()
+    } else {
+      setNeedsDisplay()
     }
+  }
     
     
-    override func draw(_ rect: CGRect) {
-        guard pointArray.count > 1 else {
-            return
-        }
-        super.draw(rect);
-        let path = UIBezierPath()
-        UIColor.black.setStroke()
-        path.move(to: pointArray[0])
-        for i in 1..<pointArray.count{
-            path.addLine(to:pointArray[i])
-        }
-        path.stroke()
- 
+  override func draw(_ rect: CGRect) {
+    super.draw(rect)
+    if preRenderedImage != nil {
+      preRenderedImage.draw(in: self.bounds)
     }
+    
+    bezierPath.lineWidth = lineWidth
+    lineColor.setStroke()
+    bezierPath.stroke()
+    
+  }
+  
+  func renderToImage() {
+    
+    UIGraphicsBeginImageContextWithOptions(self.bounds.size, false, 0.0)
+    if preRenderedImage != nil {
+      preRenderedImage.draw(in: self.bounds)
+    }
+    
+    lineColor.setStroke()
+    bezierPath.stroke()
+    
+    preRenderedImage = UIGraphicsGetImageFromCurrentImageContext()
+    
+    UIGraphicsEndImageContext()
+  }
+
+  
+  func clear() {
+    preRenderedImage = nil
+    bezierPath.removeAllPoints()
+    setNeedsDisplay()
+  }
+
 }
--- Spirograph/ViewController.swift
+++ Spirograph/ViewController.swift
@@ -10,111 +10,109 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var CanvasView: Canvas!
+  @IBOutlet weak var CanvasView: Canvas!
 
-    
-    @IBAction func swipeHappend(_ sender: UISwipeGestureRecognizer) {
-        CanvasView.pointArray.removeAll()
-        theta = 0
-        r_max = Double(arc4random_uniform(140))+0
-        r_min = Double(arc4random_uniform(140))+0
-        r_inter = Double(arc4random_uniform(60))-10
+  @IBAction func swipeHappend(_ sender: UISwipeGestureRecognizer) {
+    theta = 0
+    r_max = Double(arc4random_uniform(140)) + 0
+    r_min = Double(arc4random_uniform(140)) + 0
+    r_inter = Double(arc4random_uniform(60)) - 10
+  }
+  
+  @IBOutlet weak var stopBtn: UIButton!
+  
+  var timer: Timer!
+  var refreshTimer: Timer!
+  var r_max: Double = 100
+  var r_min: Double = 130
+  var r_inter: Double = 34
+  var theta: Double = 0
+  var delta = M_PI/40
+  var pointArray = [CGPoint]()
+  var isStopBtnTapped: Bool = false
+
+  
+  func calculateThePoint(r_max: Double, r_min: Double, r_inter: Double, theta: Double) -> CGPoint {
+    let rect: CGRect = CanvasView.bounds
+    let middle_X: CGFloat = rect.size.width / 2
+    let middle_Y: CGFloat = rect.size.height / 2
+    let x: Double = ((r_max - r_min) * cos(theta) + r_inter * cos((r_max / r_min - 1) * theta))
+    let y: Double = (-(r_max - r_min) * sin(theta) + r_inter * sin ((r_max / r_min - 1) * theta))
+    var point = CGPoint(x: x,y: y)
+    point.x = point.x + middle_X
+    point.y = point.y + middle_Y
+    return point
+  }
+  
+  
+  func sendPointToCanvasView() {
+    CanvasView.endPoint = calculateThePoint(r_max: r_max, r_min: r_min, r_inter: r_inter, theta: theta)
+    CanvasView.redraw()
+    theta = theta + delta
+  }
+  
+  func refresh() {
+    CanvasView.setNeedsDisplay()
+  }
+
+  @IBAction func R_MaxSliderChanged(_ sender: UISlider) {
+    CanvasView.clear()
+    theta = 0
+    if sender.value > 0.5 {
+      r_max = 80 + Double(sender.value * 100)
+    } else {
+      r_max = 100 - Double(sender.value * 100)
     }
-    
-    @IBOutlet weak var stopBtn: UIButton!
-    
-    var timer:Timer!
-    var refreshTimer:Timer!
-    var r_max:Double =  100
-    var r_min:Double =  130
-    var r_inter:Double = 34
-    var theta:Double = 0
-    var delta = M_PI/40
-    var pointArray = [CGPoint]()
-    var isStopBtnTapped:Bool = false
+  }
+  
+  @IBAction func R_MinSliderChanged(_ sender: UISlider) {
+    CanvasView.clear()
+    theta = 0
+    if sender.value > 0.5{
+      r_min = 80 + Double(sender.value * 100)
+      
+    } else {
+      r_min = 100 - Double(sender.value * 100)
+    }
+  }
+  
+  @IBAction func R_InterSliderChanged(_ sender: UISlider) {
+    CanvasView.clear()
+    theta = 0
+    if sender.value > 0.5{
+      r_inter = 20 + Double(sender.value * 30)
+    } else {
+      r_inter = 20 - Double(sender.value * 30)
+    }
+  }
 
+  @IBAction func stopBtnPressed(_ sender: UIButton) {
+    isStopBtnTapped = !isStopBtnTapped
+    if isStopBtnTapped{
+    stopBtn.setTitle("Resume", for: UIControlState())
+    refreshTimer.invalidate()
+    timer.invalidate()
+    } else {
+      stopBtn.setTitle("Stop", for: UIControlState())
+      timer = Timer.scheduledTimer(timeInterval: 0.001,target:self,selector:#selector(ViewController.sendPointToCanvasView),userInfo:nil,repeats:true)
     
-    func calculateThePoint(r_max:Double,r_min:Double,r_inter:Double,theta:Double)->CGPoint{
+      refreshTimer = Timer.scheduledTimer(timeInterval: 0.05,target:self,selector:#selector(ViewController.refresh),userInfo:nil,repeats:true)
+    }
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    CanvasView.startPoint = calculateThePoint(r_max: r_max, r_min: r_min, r_inter: r_inter, theta: delta)
+
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
         
-        let rect: CGRect = CanvasView.bounds
-        let middle_X:CGFloat = rect.size.width/2
-        let middle_Y:CGFloat = rect.size.height/2
-        let x:Double = ((r_max - r_min) * cos(theta) + r_inter * cos((r_max/r_min-1) * theta))
-        let y:Double = (-(r_max - r_min) * sin(theta) + r_inter * sin ((r_max/r_min-1) * theta))
-        var point = CGPoint(x:x,y:y)
-        point.x = point.x + middle_X
-        point.y = point.y + middle_Y
-        return point
-    }
-    
-    
-    func sendPointToCanvasView()
-    {
-        CanvasView.updataPoints(point: calculateThePoint(r_max: r_max, r_min: r_min, r_inter: r_inter, theta: theta))
-        theta = theta + delta
-    }
-    
-    func refresh(){
-        CanvasView.setNeedsDisplay()
-    }
-    
-    
-    @IBAction func R_MaxSliderChanged(_ sender: UISlider) {
-        CanvasView.pointArray.removeAll()
-        theta = 0
-        if sender.value > 0.5{
-            r_max = 80 + Double(sender.value * 100)}
-        else{
-            r_max = 100 - Double(sender.value * 100)
-        }
-    }
-    @IBAction func R_MinSliderChanged(_ sender: UISlider) {
-        CanvasView.pointArray.removeAll()
-        theta = 0
-        if sender.value > 0.5{
-            r_min = 80 + Double(sender.value * 100)}
-        else{
-            r_min = 100 - Double(sender.value * 100)
-        }
-    }
-    
-    @IBAction func R_InterSliderChanged(_ sender: UISlider) {
-        CanvasView.pointArray.removeAll()
-        theta = 0
-        if sender.value > 0.5{
-            r_inter = 20 + Double(sender.value * 30)}
-        else{
-            r_inter = 20 - Double(sender.value * 30)
-        }
-    }
+    timer = Timer.scheduledTimer(timeInterval: 0.0001,target:self,selector:#selector(ViewController.sendPointToCanvasView),userInfo:nil,repeats:true)
 
-    @IBAction func stopBtnPressed(_ sender: UIButton) {
-        isStopBtnTapped = !isStopBtnTapped
-        if isStopBtnTapped{
-        stopBtn.setTitle("Resume", for: UIControlState())
-        refreshTimer.invalidate()
-        timer.invalidate()
-        }else{
-            stopBtn.setTitle("Stop", for: UIControlState())
-            timer = Timer.scheduledTimer(timeInterval: 0.001,target:self,selector:#selector(ViewController.sendPointToCanvasView),userInfo:nil,repeats:true)
-            
-            refreshTimer = Timer.scheduledTimer(timeInterval: 0.05,target:self,selector:#selector(ViewController.refresh),userInfo:nil,repeats:true)
-        }
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        timer = Timer.scheduledTimer(timeInterval: 0.001,target:self,selector:#selector(ViewController.sendPointToCanvasView),userInfo:nil,repeats:true)
-
-        refreshTimer = Timer.scheduledTimer(timeInterval: 0.05,target:self,selector:#selector(ViewController.refresh),userInfo:nil,repeats:true)
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    refreshTimer = Timer.scheduledTimer(timeInterval: 0.05,target:self,selector:#selector(ViewController.refresh),userInfo:nil,repeats:true)
+  }
 }
 
